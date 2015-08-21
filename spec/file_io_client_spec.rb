0c03746bf698544900c5ab595b1b16a3e58f1209
Stefano Ordine <stefano.ordine@gmail.com>
2015-08-21 10:39:09 +0200

    test if the service is actually working

--- spec/file_io_client_spec.rb
+++ spec/file_io_client_spec.rb
@@ -4,4 +4,8 @@ describe FileIoClient do
   it 'has a version number' do
     expect(FileIoClient::VERSION).not_to be nil
   end
+
+  it "actually works" do
+    expect{FileIoClient::Upload.file('Rakefile')}.not_to raise_error
+  end
 end
